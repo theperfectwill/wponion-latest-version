ef2e4b5b59df8704a8275a9c869083d058839a64
WPOnion Downloader <wponion@gmail.com>
2020-04-27 04:11:59 +0000

    Latest WPOnion Downloaded

---
+++ wponion/index.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Plugin Name: WPOnion
+ * Plugin URI: https://github.com/wponion/
+ * Description: ~ Lightweight, Flexible & Rapid WP Development Framework ~
+ * Version: 1.4.5.3
+ * Author: varunsridharan
+ * Author URI: https://varunsridharan.in
+ * Text Domain: wponion
+ * Domain Path: /i18n/
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	die;
+}
+
+require_once 'wponion.php';
