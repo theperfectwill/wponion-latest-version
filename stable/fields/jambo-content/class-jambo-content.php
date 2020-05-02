a342ab06de01e6beef1a14ffd7f0797a47485af8
WPOnion Downloader <wponion@gmail.com>
2020-05-02 14:12:59 +0000

    WPOnion Stable & Development Source Updated

---
+++ stable/fields/jambo-content/class-jambo-content.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace WPOnion\Field;
+
+if ( ! defined( 'ABSPATH' ) ) {
+	die;
+}
+
+if ( ! class_exists( '\WPOnion\Field\Jambo_Content' ) ) {
+	/**
+	 * Class jambo_content
+	 *
+	 * @package WPOnion\Field
+	 * @author Varun Sridharan <varunsridharan23@gmail.com>
+	 * @since 1.0
+	 */
+	class Jambo_Content extends Heading {
+
+		/**
+		 * Generates Final HTML Output.
+		 *
+		 * @return mixed|void
+		 */
+		protected function output() {
+			echo $this->before();
+			echo '<div class="wpo-jumbotron">' . $this->data( 'content' ) . '</div>';
+			echo $this->after();
+		}
+	}
+}
