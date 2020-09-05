0b7a80cdded71b7be4931d234221bfffa3a5a70f
WPOnion Downloader <wponion@gmail.com>
2020-09-05 20:19:22 +0000

    WPOnion Stable & Development Source Updated

--- stable/vendor/autoload.php
+++ stable/vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInit94f3cfe8225dc639bc5a5a4a6555b9b2::getLoader();
+return ComposerAutoloaderInit684b0ed5869ede55db6db5e1ab9f2b05::getLoader();
--- stable/vendor/composer/autoload_static.php
+++ stable/vendor/composer/autoload_static.php
@@ -4,7 +4,7 @@
 
 namespace Composer\Autoload;
 
-class ComposerStaticInit94f3cfe8225dc639bc5a5a4a6555b9b2
+class ComposerStaticInit684b0ed5869ede55db6db5e1ab9f2b05
 {
     public static $files = array (
         'eba23924a915eda61344d946053e1609' => __DIR__ . '/..' . '/varunsridharan/wp-conditional-logic/src/functions.php',
@@ -35,8 +35,8 @@ class ComposerStaticInit94f3cfe8225dc639bc5a5a4a6555b9b2
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
-            $loader->prefixLengthsPsr4 = ComposerStaticInit94f3cfe8225dc639bc5a5a4a6555b9b2::$prefixLengthsPsr4;
-            $loader->prefixDirsPsr4 = ComposerStaticInit94f3cfe8225dc639bc5a5a4a6555b9b2::$prefixDirsPsr4;
+            $loader->prefixLengthsPsr4 = ComposerStaticInit684b0ed5869ede55db6db5e1ab9f2b05::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInit684b0ed5869ede55db6db5e1ab9f2b05::$prefixDirsPsr4;
 
         }, null, ClassLoader::class);
     }
