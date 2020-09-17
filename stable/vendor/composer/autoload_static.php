421d4bd136b5abb78952727e1cded04cc3c2bb9f
WPOnion Downloader <wponion@gmail.com>
2020-09-17 12:23:53 +0000

    WPOnion Stable & Development Source Updated

--- stable/vendor/composer/autoload_static.php
+++ stable/vendor/composer/autoload_static.php
@@ -4,7 +4,7 @@
 
 namespace Composer\Autoload;
 
-class ComposerStaticInit35ba2602dabcab38b0df5a7cbf561472
+class ComposerStaticInitfcee5a231314568f02ce2e6e9b2a76b9
 {
     public static $files = array (
         'eba23924a915eda61344d946053e1609' => __DIR__ . '/..' . '/varunsridharan/wp-conditional-logic/src/functions.php',
@@ -35,8 +35,8 @@ class ComposerStaticInit35ba2602dabcab38b0df5a7cbf561472
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
-            $loader->prefixLengthsPsr4 = ComposerStaticInit35ba2602dabcab38b0df5a7cbf561472::$prefixLengthsPsr4;
-            $loader->prefixDirsPsr4 = ComposerStaticInit35ba2602dabcab38b0df5a7cbf561472::$prefixDirsPsr4;
+            $loader->prefixLengthsPsr4 = ComposerStaticInitfcee5a231314568f02ce2e6e9b2a76b9::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInitfcee5a231314568f02ce2e6e9b2a76b9::$prefixDirsPsr4;
 
         }, null, ClassLoader::class);
     }
