538ac43f7f51e77ca83963653b1ca4e89a4a850d
WPOnion Downloader <wponion@gmail.com>
2021-02-11 12:27:40 +0000

    WPOnion Stable & Development Source Updated

--- stable/vendor/autoload.php
+++ stable/vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInita73956bf8052d268e5d07093bd79e98e::getLoader();
+return ComposerAutoloaderInit0889166a7b41cd47d258dcf495a02dfc::getLoader();
