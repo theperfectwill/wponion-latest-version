267bba28541421a2adfc4f34f4cfe7ef994f5a57
WPOnion Downloader <wponion@gmail.com>
2021-09-20 16:29:30 +0000

    WPOnion Stable & Development Source Updated

--- stable/vendor/autoload.php
+++ stable/vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInit4d7dbd8c5ba86f885d0b90c29776d4b9::getLoader();
+return ComposerAutoloaderInit56c59f3118c125a6a267a49c25793c4a::getLoader();
--- stable/vendor/composer/autoload_static.php
+++ stable/vendor/composer/autoload_static.php
@@ -4,12 +4,12 @@
 
 namespace Composer\Autoload;
 
-class ComposerStaticInit4d7dbd8c5ba86f885d0b90c29776d4b9
+class ComposerStaticInit56c59f3118c125a6a267a49c25793c4a
 {
     public static $files = array (
+        '011316cc7fd7a11c4bebeb6bcdea5621' => __DIR__ . '/..' . '/varunsridharan/wp-dependencies/src/dependencies.php',
         'eba23924a915eda61344d946053e1609' => __DIR__ . '/..' . '/varunsridharan/wp-conditional-logic/src/functions.php',
         '0435a58c821d590b8a56387329899da3' => __DIR__ . '/..' . '/varunsridharan/wp-conditional-logic/src/WP_Conditional_Logic.php',
-        '011316cc7fd7a11c4bebeb6bcdea5621' => __DIR__ . '/..' . '/varunsridharan/wp-dependencies/src/dependencies.php',
         '2a9c288188fe6d81555feec2fa6ec428' => __DIR__ . '/../..' . '/index.php',
     );
 
@@ -39,9 +39,9 @@ class ComposerStaticInit4d7dbd8c5ba86f885d0b90c29776d4b9
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
-            $loader->prefixLengthsPsr4 = ComposerStaticInit4d7dbd8c5ba86f885d0b90c29776d4b9::$prefixLengthsPsr4;
-            $loader->prefixDirsPsr4 = ComposerStaticInit4d7dbd8c5ba86f885d0b90c29776d4b9::$prefixDirsPsr4;
-            $loader->classMap = ComposerStaticInit4d7dbd8c5ba86f885d0b90c29776d4b9::$classMap;
+            $loader->prefixLengthsPsr4 = ComposerStaticInit56c59f3118c125a6a267a49c25793c4a::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInit56c59f3118c125a6a267a49c25793c4a::$prefixDirsPsr4;
+            $loader->classMap = ComposerStaticInit56c59f3118c125a6a267a49c25793c4a::$classMap;
 
         }, null, ClassLoader::class);
     }
